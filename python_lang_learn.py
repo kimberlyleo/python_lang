dac1c05569cff8ceed420f74893cfc6d1a9197d8
Kimberly <kimberly.leonard@mjdinteractive.com>
2020-06-24 10:25:31 -0700

    added int float examples

--- python_lang_learn.py
+++ python_lang_learn.py
@@ -6,13 +6,47 @@
 # loops 
 # recursive function example
 
+
 # SYNTAX 
 # Language Basics: PYTHON is an interpretted (not compiled), object oriented, high level, language. their site also says it has dynamic semantics which means how the program's code is executed changes with new information... need to learn more about this and how it works!!
 
 # OPERATORS (will add bitwise operators)
 # arithmetic operators: +, -, *, /, %, **, // (floor division)
-# assignment operators: =, +=, -=, *=, /=, //=, etc.
+def floor_division(num):
+    quotient_rounded_down = num//2
+    return quotient_rounded_down
+
+# Aassignment Operators: =, +=, -=, *=, /=, //=, etc.
+def assignment_actions(num_input):
+    num = num_input
+    print(num) # >> 2
+    num *= num
+    print(num) # >> 4
+    num /= num 
+    print(num) # >> 1.0 
+    num += num 
+    print(num) # >> 2.0
+    num **= num
+    print(num) # >> 4.0
+    num //= num #this does NOT cause type conversion from int to a float
+    print(num) # >> 1.0
+    num -= num
+    print(num) # >> 0.0
+    
+    #notice that division operation always creates a float quotient (more on ints and floats next)
+#assignment_actions(2)
+
+# Integers and floats
+#examples showing python's built in number/float functions inc. type conversions 
+#https://www.tutorialspoint.com/python/python_numbers.htm
+assert int(3.0) == 3
+
+assert float(3) == 3.0
+
+
 # comparison operators: ==, !=, >, >=, <,<= used to create boolean value/ also referred to as bool operators
+assert 2 + 2 == 4 
+    #assert is used to ensure that a comparison operation returns True (nothing will happen if True); if False an AsserionAError is raised
 # identity operators: in : used to see if it is member or subtype of a given class is / is not ; used to evaluate if identity is identical (same object occupying the same place in memory)
 
 
