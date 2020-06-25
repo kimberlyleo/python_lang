a2da564e775162782be96e583c84993e2fc25242
Kimberly <kimberly.leonard@mjdinteractive.com>
2020-06-25 09:27:46 -0700

    file structure changed

--- python_lang_learn.py
+++ python_lang_learn.py
@@ -130,6 +130,7 @@ assert list(range(0, 30, 10)) == [0, 10, 20]
 #Slice end value index position is not inclusive
 oreo_flavors = ['Mint Chocolate', 'Birthday Cake', 'Peanut Butter', 'Original']
 assert oreo_flavors[-2:] == ['Peanut Butter', 'Original']
+#negative index position starts at the end of the list and counts inward
 assert oreo_flavors[0:2] == ['Mint Chocolate', 'Birthday Cake']
 assert oreo_flavors[3] == "Original"
 
@@ -169,53 +170,3 @@ assert list(accumulated_rain.items()) == [('Carlsbad', 2), ('Cloud Forest', 44)]
 
 
 
-# Functions
-# illustrated with examples below
-
-def dew_point_calc(temp_celsius, relative_humidity_percent):
-    dew_point_temperature = temp_celsius - ((100 - relative_humidity_percent)/5)
-    return dew_point_temperature
-
-def submit_to_overlord_Goodburger():
-    user_response = input("Welcome to Goodburger, home of the Goodburger. Would you like a Goodburger? \n\'y\' OR \'n\'\n")
-    if (user_response != "y"):
-        submit_to_overlord_Goodburger()
-
-#submit_to_overlord_Goodburger()
-
-# Loops
-
-def while_loop():
-    while True:
-        print("This will continue to loop until break conditions are met")
-        user_response = int(input("Pick a number between 1 and 5\n"))
-        if (user_response in [1, 2, 3, 4, 5]):
-            break
-    if (user_response % 2 == 0):
-        return "Great job with {}, Even Steven".format(str(user_response))
-    elif (user_response == 1) and (user_response != 5):
-        return "winner winner second place"
-    elif user_response != 5 or user_response != 3:
-        return "this is a pointless conditional to show \'or\' comparison operator"
-
-#print(while_loop())
-
-def while_counter_runs():
-    counter = 0
-    while (counter < 5):
-        print("Counter value is {}".format(str(counter)))
-        counter += 1
-        #because the counter increases after print statement, counter values printed range from 0 to 4
-        #if the counter increase was above print statement range printed would be 1 to 4
-
-# while_counter_runs()
-
-cat_names = ["Fyodor", "Reggie", "Socks", "Yarnell"]
-
-def list_loop(names):
-    cat_number = 1
-    for cat in names: 
-        print("{}. {} ".format(cat_number, cat))
-        cat_number += 1
-
-#list_loop(cat_names)
